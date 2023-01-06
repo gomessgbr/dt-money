2aeb7711673fe74a7b82e7cb146ef1849bfa07a0
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-06 15:30:33 -0300

    feat: creata a table on Transaction Page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,15 +1,32 @@
-import {TransactionContainer} from './styles'
+import { TransactionContainer, TransactionTable } from "./styles";
 
-import { Header } from '../../components/Header'
-import { Summary } from '../../components/Summary'
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
+export function Transaction() {
+  return (
+    <div>
+      <Header />
+      <Summary />
 
-export function Transaction(){
-    return(
-        <TransactionContainer>
-            <Header/>
-            <Summary/>
-            
-        </TransactionContainer>
-    )
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>- R$ 59,00</td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
+    </div>
+  );
 }
