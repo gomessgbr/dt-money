1bb1028b66b21ad75b745a53d1927b1ea7e91f6b
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-06 14:18:00 -0300

    feat: Header on Transaction Page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,12 @@
 import {TransactionContainer} from './styles'
 
+import { Header } from '../../components/Header'
+
 
 export function Transaction(){
     return(
         <TransactionContainer>
+            <Header/>
             
         </TransactionContainer>
     )
