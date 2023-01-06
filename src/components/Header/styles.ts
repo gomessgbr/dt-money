ba4ccb299956516e9925fdddfbcf4320e336c1c6
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-06 20:39:32 -0300

    lint

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -11,26 +11,23 @@ export const HeaderContent = styled.div`
   margin: 0 auto;
   padding: 0 1.5rem;
 
-  
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
 export const NewTransactionButton = styled.button`
-
   height: 50px;
   border: 0;
-  background: ${props=>props.theme['green-500']};
-  color: ${props=>props.theme.white};
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme.white};
   font-weight: bold;
   padding: 0 1.25rem;
   border-radius: 6px;
   cursor: pointer;
-  
 
-  &:hover{
-    background: ${props=>props.theme['green-700']};
+  &:hover {
+    background: ${(props) => props.theme["green-700"]};
     transition: background-color 0.2s;
   }
-`
+`;
