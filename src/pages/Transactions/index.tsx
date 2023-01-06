6e3dcdfb2b91bae2ac475338c4cb83f4554eb969
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-06 14:58:30 -0300

    feat: add Summary component on TransactionPage

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,14 @@
 import {TransactionContainer} from './styles'
 
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
 
 
 export function Transaction(){
     return(
         <TransactionContainer>
             <Header/>
+            <Summary/>
             
         </TransactionContainer>
     )
