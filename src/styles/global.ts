c8dc499a00af90e0fc68bbfcc98710ed54cff477
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-07 12:33:41 -0300

    lint: global spaces

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,14 +5,12 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-
-        
-}
+    }
 
 :focus{
         outline: 0;
-        box-shadow:  0 0 0 2px ${(props) => props.theme["green-500"]};
-        }
+    box-shadow:  0 0 0 2px ${(props) => props.theme["green-500"]};
+    }
 
         body{
             background-color: ${(props) => props.theme["gray-800"]};
