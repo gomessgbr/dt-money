b4961c2eada292eb5494ad7bab062834fa1bbbd2
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-07 13:26:39 -0300

    refactor: transactions on Transaction Page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -7,30 +7,11 @@ import {
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
-import { useEffect, useState } from "react";
-
-interface Transaction {
-  id: number;
-  description: string;
-  type: "income" | "outcome";
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionContext";
 
 export function Transaction() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransaction() {
-    const response = await fetch("http://localhost:3000/transactions");
-    const data = await response.json();
-    setTransactions(data);
-  }
-
-  useEffect(() => {
-    loadTransaction();
-  }, []);
-
+  const { transactions } = useContext(TransactionsContext);
   return (
     <div>
       <Header />
