6257f933c144ffb50d5dbbfe982c46fa69877b0e
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-07 13:26:22 -0300

    feat: add provider between transaction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-
-import { ThemeProvider } from 'styled-components'
-import { Transaction } from './pages/Transactions'
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from "styled-components";
+import { Transaction } from "./pages/Transactions";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./contexts/TransactionContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle/>
-      <Transaction/>
+      <GlobalStyle />
+      <TransactionsProvider>
+        <Transaction />
+      </TransactionsProvider>
     </ThemeProvider>
-  )
+  );
 }
-
-
