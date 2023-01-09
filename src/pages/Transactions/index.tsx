0780d3d7f78030355c0ba6111e97cf157fac0e19
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-09 19:28:12 -0300

    refactor: useContext using contextSelector

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -7,12 +7,15 @@ import {
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { SearchForm } from './components/SearchForm'
-import { useContext } from 'react'
+
 import { TransactionsContext } from '../../contexts/TransactionContext'
 import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { useContextSelector } from 'use-context-selector'
 
 export function Transaction() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
   return (
     <div>
       <Header />
