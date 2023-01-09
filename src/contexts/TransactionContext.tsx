00a4e8c972c782c0fc0dd5c3a3d8bf8742cc25d0
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-09 18:27:01 -0300

    feat: new function to create a new transaction

--- src/contexts/TransactionContext.tsx
+++ src/contexts/TransactionContext.tsx
@@ -10,9 +10,17 @@ interface Transaction {
   createdAt: string;
 }
 
+interface CreateTransactionInput {
+  description: string;
+  price: number;
+  category: string;
+  type: "income" | "outcome";
+}
+
 interface TransactionContextType {
   transactions: Transaction[];
-  fetchTransactions: (query? :string) => Promise<void>;
+  fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data:CreateTransactionInput) => Promise<void>
 }
 
 interface TransactionsProviderProps {
@@ -24,21 +32,36 @@ export const TransactionsContext = createContext({} as TransactionContextType);
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions(query? : string) {
-
-    const response = await api.get('/transactions',{
+  async function fetchTransactions(query?: string) {
+    const response = await api.get("/transactions", {
       params: {
-        q:query
-      }
-    })
+        _sort: "createdAt",
+        _order: "cresc",
+        q: query,
+      },
+    });
     setTransactions(response.data);
+  }
+
+  async function createTransaction(data: CreateTransactionInput) {
+    const { category, description, price, type } = data;
+
+    const response = await api.post("transactions", {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date(),
+    });
+
+    setTransactions((state) => [...state, response.data]);
   }
 
   useEffect(() => {
     fetchTransactions();
   }, []);
   return (
-    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   );
